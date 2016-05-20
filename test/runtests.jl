1d902c0531c14068b30f52f13224eaa9775cc963
Christoph Ortner <c.ortner@warwick.ac.uk>
2016-05-20 22:33:34 +0100

    squashed a couple of commits trying to fix travis

--- test/runtests.jl
+++ test/runtests.jl
@@ -62,7 +62,7 @@ println("=================================================")
 println("Test 4: AMG as a preconditioner")
 println("        100 x 100 Dirichlet problem, TOL = 1e-4")
 println("        PyAMG vs CG vs PCG  (using IterativeSolvers)")
-A, b = L3d(100)
+A, b = L2d(100)
 amg = RugeStubenSolver(A)
 M = aspreconditioner(amg)
 using IterativeSolvers
@@ -71,8 +71,15 @@ println("Plain CG:")
 @time x_cg, _ = IterativeSolvers.cg(A, b, 1; tol=TOL)
 @time x_cg, _ = IterativeSolvers.cg(A, b, 1; tol=TOL)
 println("PyAMG-preconditionerd CG:  (see `aspreconditioner`)")
-@time x_pcg = IterativeSolvers.cg(A, b, M; tol=TOL)
-@time x_pcg = IterativeSolvers.cg(A, b, M; tol=TOL)
+@time x_pcg, _ = IterativeSolvers.cg(A, b, M; tol=TOL)
+@time x_pcg, _ = IterativeSolvers.cg(A, b, M; tol=TOL)
 println("PyAMG solver")
-@time x_pyamg = PyAMG.solve(amg, b; tol=TOL, accel="cg")
-@time x_pyamg = PyAMG.solve(amg, b; tol=TOL, accel="cg")
+@time x_pyamg = PyAMG.solve(amg, b; tol=TOL*1e-2, accel="cg")
+@time x_pyamg = PyAMG.solve(amg, b; tol=TOL*1e-2, accel="cg")
+x = A \ b
+println("|x_cg-x| = ", norm(x_cg - x), " \n",
+         "|x_pcg-x| = ", norm(x_pcg - x), "\n",
+         "|x_pyamg-x| = ", norm(x_pyamg - x) )
+@assert norm(x_cg - x) < 1e-5
+@assert norm(x_pcg - x) < 1e-5
+@assert norm(x_pyamg - x) < 1e-5
